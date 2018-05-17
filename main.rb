807f3ce73fed871bb37ecd8042f2dacde780e1ba
nagatech <cheesecakebot@yahoo.co.jp>
2018-05-17 23:46:36 +0900

    new commit

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require "twitter"
 #require "dotenv"
+require "./time.rb"
 
 #Dotenv.load
 
@@ -24,17 +25,30 @@ cheese_client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["MY_ACCESS_TOKEN_SECRET2"]
 end
 
-random = Random.new
+retry_count ||= 0
 
-nagatech_stream_client.filter(track: "#ながてちキャンペーン") do |tweet|
-  if tweet.is_a?(Twitter::Tweet)
-    number = random.rand(0..33)
-    if number == 0
-      hantei = "atari"
-      cheese_client.update("@syobon_titech\n@#{tweet.user.screen_name} さんが当選しました")      
-    else
-      hantei = "hazure"
+begin
+  random = Random.new
+
+  nagatech_stream_client.filter(track: "#ながてちキャンペーン") do |tweet|
+    if tweet.is_a?(Twitter::Tweet)
+      number = random.rand(0..33)
+      if number == 0
+        hantei = "atari"
+        cheese_client.update("@syobon_titech\n@#{tweet.user.screen_name} さんが当選しました")      
+      else
+        hantei = "hazure"
+      end
+      nagatech_client.update_with_media("@#{tweet.user.screen_name}\nご応募ありがとうございます！\n気になる抽選の結果は…！？\n当選するまで何度でも挑戦できますよ♪\n6/14（木）23:59まで！\n\nキャンペーンの詳しい詳細はこちらから！\nhttps://blog.nagatech.work/nagatech-campaign\n", File.open("./res/#{hantei}.png"), options = {:in_reply_to_status_id => tweet.id})
     end
-    nagatech_client.update_with_media("@#{tweet.user.screen_name}\nご応募ありがとうございます！\n気になる抽選の結果は…！？\n当選するまで何度でも挑戦できますよ♪\n6/14（木）23:59まで！\n\nキャンペーンの詳しい詳細はこちらから！\nhttps://blog.nagatech.work/nagatech-campaign\n", File.open("./res/#{hantei}.png"), options = {:in_reply_to_status_id => tweet.id})
+  end
+rescue => exception
+  retry_count += 1
+  if retry_count < 7
+    #client2.update("エラー発生(リトライ回数#{retry_count}回目)\n#{exception.message}\n#{JSTTime.timecode}")
+    puts("エラー発生(リトライ回数#{retry_count}回目)\n#{exception.message}\n#{exception.backtrace}\n#{JSTTime.timecode}")
+    retry if retry_count < 7
+  else
+    #client.update("@syobon_titech\nエラー発生\nリトライ回数が規定回数を超えたので強制終了します。\n#{exception.message}\n#{JSTTime.timecode}")
   end
 end
